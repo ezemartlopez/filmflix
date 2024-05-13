28ed979f75116c000eaee34fead58e8f154177c1
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-12 21:32:29 -0300

    correccion de indentacion.

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from '../pages/Home';
 import Base from '../pages/Base';
 import View from '../pages/View';
 
+
 function RouterApp() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Base />} >
-                <Route path="" element={<Home/>} />
-                <Route path=":id" element={<View/>} />
-            </Route>
+      <Routes>
+        <Route path="/" element={<Base />} >
+          <Route path="" element={<Home/>} />
+          <Route path=":id" element={<View/>} />
+        </Route>
         {/* ... otras rutas */}
-        </Routes>
+      </Routes>
     </BrowserRouter>
   )
 }
